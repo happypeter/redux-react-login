22c4b46a16e873f8320e0979e2aed45fb772913f
Peter Wang <happypeter1983@gmail.com>
2017-07-19 09:36:44 +0800

    two components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import PostBody from './PostBody'
+import CommentBox from './CommentBox'
 
 class App extends Component {
   render() {
     return (
       <div>
-        hello
+        <PostBody />
+        <CommentBox />
       </div>
     );
   }
---
+++ src/PostBody.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostBody extends Component {
+  render() {
+    return (
+      <div>
+        PostBody
+      </div>
+    );
+  }
+}
+
+export default PostBody
