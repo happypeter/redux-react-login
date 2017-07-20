55105225377d8a9bebeeec689aafe01ee3dceb78
Peter Wang <happypeter1983@gmail.com>
2017-07-20 16:36:14 +0800

    homepage style

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react'
 import {
   Link
 } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PostBody from './PostBody'
 
 class HomePage extends Component {
 
   render() {
+    let { posts } = this.props
+    console.log('posts', posts)
+    let postList = posts.map(item => {
+      return(
+        <PostBody key={item.postId} postId={item.postId} />
+      )
+    })
     return (
-      <div>
-        <ul>
-          <li> <Link to="/post/1">Git 技巧</Link> </li>
-          <li> <Link to="/post/2">学习 Redux</Link> </li>
-        </ul>
+      <div className="home">
+        { postList }
       </div>
     )
   }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => ({
+  posts: state.posts
+})
+
+export default connect(mapStateToProps)(HomePage)
