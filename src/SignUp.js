0f56c22abb7a95c934d8ae1aee9e1a2b2ffe8100
Peter Wang <happypeter1983@gmail.com>
2017-07-21 11:47:02 +0800

    onSubmit

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
 class SignUp extends Component {
+  style = {
+    'width': '300px',
+    'margin': '20px auto'
+  }
+
+  signUp = (e) => {
+    e.preventDefault()
+    let username = this.usernameInput.value
+    let password = this.passwordInput.value
+    console.log({username, password})
+  }
+
   render() {
     return(
-      <div>
-        SignUp
+      <div style={this.style}>
+        <h1>注册</h1>
+        <form onSubmit={this.signUp}>
+          <div>
+            <input ref={value => this.usernameInput = value} type="text" placeholder="用户名" />
+          </div>
+          <div>
+            <input ref={value => this.passwordInput = value} type="password" placeholder="password" />
+          </div>
+          <div>
+            <input type="password" placeholder="再输入一次" />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
       </div>
     )
   }
